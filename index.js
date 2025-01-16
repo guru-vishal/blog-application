3fa7db3e11c824a9697064b5cf0110043628349a
guru-vishal <vishal3012006@gmail.com>
2025-01-16 12:53:24 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from "express";                                  // Import express package
+
+const app = express();                                          // Instance for express
+const port = 3000;                                              // Set port as 3000
+
+let posts = [];                                                 // Handle posts
+
+app.use(express.static("public"));                              // Access public folder
+app.use(express.urlencoded({extended: true}));                  // Handle actions like form submissions
+
+app.get("/", (req, res) => {                                    // 'Home' page
+    res.render("index.ejs");
+});
+
+app.get("/post", (req, res) => {                                // 'Create a new post' page
+    res.render("post.ejs");
+});
+
+app.get("/view", (req, res) => {                                // 'View posts' page
+    res.render("view.ejs", {posts});
+});
+
+app.get("/update", (req, res) => {                              // 'Update a post' page
+    res.render("update.ejs", {posts});
+});
+
+app.get("/delete", (req, res) => {                              // 'Delete a post' page
+    res.render("delete.ejs", {posts});
+});
+
+app.get("/about", (req, res) => {                               // 'About' page
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {                             // 'Contact' page
+    res.render("contact.ejs")
+});
+
+app.post("/create", (req, res) => {                             // Add new post
+    const currentDate = new Date();
+    const newPost = {
+        id: posts.length+1,
+        title: req.body.title,
+        content: req.body.content,
+        datePosted: currentDate.toLocaleString(),
+        dateUpdated: null
+    };
+    posts.push(newPost);
+    res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {                               // Edit a post
+    const currentDate = new Date();
+    const postId = parseInt(req.body.id, 10);
+    const post = posts.find(p => p.id === postId);
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.dateUpdated = currentDate.toLocaleString();
+    res.redirect("/");
+});
+
+app.post("/remove", (req, res) => {                             // Delete a post
+    const postId = parseInt(req.body.id, 10);
+    const postIndex = posts.findIndex(p => p.id === postId);
+    posts.splice(postIndex, 1);
+    res.redirect("/");
+});
+
+app.listen(port, (req, res) => {                                // Listen the server on the port
+    console.log(`Server running on port ${port}`);
+});
+
